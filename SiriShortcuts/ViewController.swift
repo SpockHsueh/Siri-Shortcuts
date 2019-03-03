4b125dc4477da197e2d6e86e9471453a8234ac85
Spoke <spock.hsu@gmail.com>
2019-03-03 21:52:20 +0800

    first commit

---
+++ SiriShortcuts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SiriShortcuts
+//
+//  Created by Spoke on 2019/3/3.
+//  Copyright © 2019年 Spock. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
